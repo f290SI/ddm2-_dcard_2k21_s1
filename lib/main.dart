9c9ea277d31fbaf8f391384baae02dc7d2e9c3f0
Esdras Bezerra da Silva <esdrasilva@gmail.com>
2021-04-08 22:25:02 -0300

    Inclusao de comentarios

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,19 @@ import 'package:url_launcher/url_launcher.dart';
 main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  // Endereço de imagem para uso no NetworkImage [ImageProvider]
   final imageUrl =
       'https://images.pexels.com/photos/2761930/pexels-photo-2761930.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
+  // Cores utilizadas no App
   final darkBlue = Color.fromARGB(255, 18, 32, 47);
   final pinkAccentA200 = Colors.pinkAccent.shade200;
   final linkedInColor = Color.fromARGB(255, 40, 103, 178);
   final facebookColor = Color.fromARGB(255, 66, 103, 178);
   final facebook = Color(0xFF4267B2);
 
+  ///Função para abrir um WebView para acessar as paginas de portfolio e redes sociais
   Future<void> _launchInWebView(String url) async {
     if (await canLaunch(url)) {
       await launch(url, forceWebView: true);
@@ -25,6 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Configuração do tema escuro alterando a cor de fundo do Scaffold e o estilo do FAB(FloatingActionButton)
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: darkBlue,
         floatingActionButtonTheme: FloatingActionButtonThemeData(
@@ -40,6 +45,7 @@ class MyApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            // Widget de Imagemm Circular
             CircleAvatar(
               radius: 80,
               backgroundColor: pinkAccentA200,
@@ -48,6 +54,7 @@ class MyApp extends StatelessWidget {
                 backgroundImage: NetworkImage(imageUrl),
               ),
             ),
+            // Espaçamento entre Widgets
             SizedBox(
               height: 24,
               width: double.infinity,
@@ -55,6 +62,7 @@ class MyApp extends StatelessWidget {
             Text(
               'Larissa Belloni',
               style: TextStyle(
+                //Uso de fonte customizada por recurso adicional via pubspec.
                 fontSize: 48, fontFamily: 'Devonshire',
               ),
             ),
@@ -73,17 +81,21 @@ class MyApp extends StatelessWidget {
             SizedBox(
               height: 26,
             ),
+            //Criação de card
             Card(
               color: pinkAccentA200,
               margin: EdgeInsets.symmetric(vertical: 10, horizontal: 32),
               elevation: 8,
+              //Inclusao de WidGet ListTile para estruturar o icone a esquerda e o texto no centro sem utilização de Row()
               child: ListTile(
                 leading: Icon(FontAwesomeIcons.github),
                 title: Text('Meu Portfólio no GitHub'),
                 onTap: () {
+                  //Chamada de funcção de abertura de pagina web pelo otque no ListTile
                   _launchInWebView('https://www.github.com/BZR4');
                 },
               ),
+              //Arredondar os cantos do Card
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50),),
             ),
             Card(
